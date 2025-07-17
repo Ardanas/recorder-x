f36e80ff022a4eb70a9e76b006e898d427f603a4
ardans <ardans@foxmail.com>
2025-07-17 11:00:18 +0800

    refactor(panel): 优化面板列表组件逻辑，提取预览记录项功能
    重构面板列表组件，将记录项的预览逻辑提取到单独的函数中。使用计算属性生成处理后的记录列表，简化模板中的复杂表达式，提高代码可读性和维护性。

--- components/business/panel/PanelList.vue
+++ components/business/panel/PanelList.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from '~/components/ui/card';
-import { Avatar, AvatarImage, AvatarFallback } from '~/components/ui/avatar';
-import { Record } from '~/utils/types';
-import { PanelTitleEdit, PanelImage } from '~/components/business/panel';
+import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card';
+import { Record, RecordItem } from '~/utils/types';
+import { PanelImage } from '~/components/business/panel';
 import CardFooter from '@/components/ui/card/CardFooter.vue';
 import { Separator } from '@/components/ui/separator';
 import { getRelativeTime } from '~/utils/time';
@@ -14,28 +13,45 @@ const props = defineProps<{
 const emits = defineEmits<{
   (e: 'select', record: Record): void;
 }>();
+
+function findBestRecordItem(items: RecordItem[]) {
+  return items.find(item => !!item.url) || items[0]
+}
+
+const recordList = computed(() => {
+  return props.list.map((record) => {
+    const { url, position, size } = findBestRecordItem(record.items)
+    return {
+      origin: record,
+      itemTotal: record.items.length,
+      previewUrl: url,
+      previewPosition: position,
+      previewSize: size
+    }
+  })
+})
 </script>
 
 <template>
   <div class="grid grid-cols-1 xl:grid-cols-2 4xl:grid-cols-4 gap-4 py-6">
-    <Card v-for="record in props.list" :key="record.id" class="hover:shadow-md flex flex-col cursor-pointer" @click="emits('select', record)">
+    <Card v-for="record in recordList" :key="record.origin.id" class="hover:shadow-md flex flex-col cursor-pointer" @click="emits('select', record.origin)">
       <CardHeader class="p-3">
         <CardTitle class="text-lg font-medium">
-          {{ record.title }}
+          {{ record.origin.title }}
         </CardTitle>
       </CardHeader>
       <CardContent class="p-3 pt-0 flex-1">
         <PanelImage
-          :src="record.items[0].url"
-          :position="record.items[0].position"
-          :size="record.items[0].size"
+          :src="record.previewUrl"
+          :position="record.previewPosition"
+          :size="record.previewSize"
         />
       </CardContent>
       <CardFooter class="p-3">
         <div class="flex h-5 items-center space-x-4 text-sm text-gray-500">
-          <span>{{ record.items.length }} 条</span>
+          <span>{{ record.itemTotal }} 条</span>
           <Separator orientation="vertical" />
-          <span>{{ getRelativeTime(record.createdAt) }}</span>
+          <span>{{ getRelativeTime(record.origin.createdAt) }}</span>
         </div>
       </CardFooter>
     </Card>
